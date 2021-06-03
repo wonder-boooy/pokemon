e8116f55a16b82fc98f8db1de127e545d3ec6b6e
yuma.wada <yuma.wada.1127@gmail.com>
2021-06-03 10:27:34 +0900

    Update: GETしたポケモンは更新ではなく追加に変更＆クリアボタン設置

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -4,27 +4,29 @@ import axios from 'axios'
 export const Pokemon = () => {
   // state定義
   const [name, setName] = useState('ポケモン');
-  const [images, setImages] = useState(['https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Pok%C3%A9ball.png/200px-Pok%C3%A9ball.png'])
+  const [images, setImages] = useState<string[]>([])
   // スタイル定義
   const buttonStyle = {
     border: 'none',
     height: '36px',
     borderRadius: '8px',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    margin: '8px',
+    padding: '8px',
   };
   // ポケモンの画像を取得するaxios
-  const getPokemon = async () => {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+  const getPokemon = async (pokemon: string) => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     return response;
   }
-  // ボタンクリック時の挙動
-  const onClick = () => {
-    getPokemon()
+  // ポケモンGETだぜボタンクリック時の挙動
+  const onClick = (pokemon: string) => {
+    getPokemon(pokemon)
       .then((res) => {
         console.log(res)
         const pokeName = res.data.name;
         const pokeImages = res.data.sprites.versions;
-        const pokeImagesURL: string[] = [];
+        const pokeImagesURL: string[] = [...images];
         Object.keys(pokeImages).map((key) => {
           return Object.keys(pokeImages[key]).map((key2) => {
             return pokeImagesURL.push(pokeImages[key][key2]["front_default"]);
@@ -35,13 +37,22 @@ export const Pokemon = () => {
         setImages(pokeImagesURL);
       });
   };
+  // クリアボタン押下時の挙動
+  const onClickClear = () => {
+    setImages([]);
+  }
   // 画面描画
   return (
     <>
       <h1>歴代の {name}, GETだぜ！</h1>
-      <button style={buttonStyle} onClick={onClick}>ポケモン、GETだぜ！</button>
+      <button style={buttonStyle} onClick={onClickClear}>クリア</button>
       <div className="flex-container">
-        {images.map((image, index) => {
+        <button className="flex-item" style={buttonStyle} onClick={() => onClick('pikachu')}>ポケモン、GETだぜ！</button>
+        <button className="flex-item" style={buttonStyle} onClick={() => onClick('pikachu')}>ポケモン、GETだぜ！</button>
+        <button className="flex-item" style={buttonStyle} onClick={() => onClick('pikachu')}>ポケモン、GETだぜ！</button>
+      </div>
+      <div className="flex-container">
+        {images && images.map((image, index) => {
           return <img key={index} sizes="1vw" src={image} alt="alt" />
         })}
       </div>
